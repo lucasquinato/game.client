21245f51869d71575482e331d847d5918f476f7c
lucasquinato <lucasquinato2015@gmail.com>
2025-12-11 05:38:05 -0300

    feat(project): adicionado primeiros arquivos

---
+++ src/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded",
+    async function(e) {
+        try {
+            //
+        } catch (error) {
+            console.error("Erros em tempo de execução:");
+            console.error(error);
+        }
+    }
+);
